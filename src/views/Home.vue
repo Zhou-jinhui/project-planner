d7bb454baf6f7aff18210925f2002ca23c09b899
Cassiopeia <86bouledeneige@gmail.com>
2021-06-17 15:38:41 +0800

    finished DELETE and COMPLETE functionality of SingleProject component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="home">
-    HOME
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue';
+
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((item) => {
+        return item.id !== id;
+      });
+    },
+    handleComplete(id) {
+      const p = this.projects.find((item) => {
+        return item.id === id;
+      });
+      p.complete = !p.complete;
+    },
+  },
 };
 </script>
